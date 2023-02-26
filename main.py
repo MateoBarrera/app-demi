30489dbecefa1973db922456eeb8c9aa1470e278
MateoBarrera <materile1997@gmail.com>
2023-02-26 16:19:34 -0500

    ADD robot.txt

--- main.py
+++ main.py
@@ -393,6 +393,19 @@ def consulta():
     return render_template("consulta.html", **context)
 
 
+@app.route('/robots.txt')
+def noindex():
+    """This file restricts the activity of search engine crawlers and stop their access to app.
+
+    Returns:
+        HTTP response: text plain response
+    """
+    r = Response(response="User-Agent: *\nAllow: /\nDisallow: /terapia/",
+                 status=200, mimetype="text/plain")
+    r.headers["Content-Type"] = "text/plain; charset=utf-8"
+    return r
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
     # socketio.run(app, host='0.0.0.0', debug=True)
